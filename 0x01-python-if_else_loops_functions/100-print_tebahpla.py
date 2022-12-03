2e263369969960b8ebdf731233682e83c92ad367
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-03 17:49:15 +0100

    Done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,5 +2,5 @@
 lower = list(range(122, 96, -1))
 upper = list(range(90, 64, -1))
 for i, j in zip(lower, upper):
-    print(chr(i) + chr(j), end="")
+    print(chr(i).format() + chr(j), end="")
 print("")
