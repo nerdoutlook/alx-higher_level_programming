8b0cd1cfadc00c220b067483d90f284b7f18d0ab
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-05 00:20:40 +0100

    Added loop statement for test case of 1 argument

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,14 @@ if __name__ == "__main__":
     import sys
 
     argv = sys.argv
-    a = len(argv)
+    a = len(argv) - 1
     j = 1
-    
-    if a == 1:
+
+    if a == 0:
         print("0 arguments.".format(a))
+    elif a == 1:
+        print("{} argument:".format(a))
+        print("{}: {}".format(j, argv[1]))
     else:
         print("{} arguments:".format(a))
         for i in argv[1:]:
