cf701cd01400a94c7e32e044d26a35204807f55d
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-04 18:13:51 +0100

    Added print statement to except statements

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,14 +3,16 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     a = 10
-    b = 5
+    b = "5"
 
     try:
         a / 1 == a
         b / 1 == b
-    except ValueError:
+    except ValueError as ve:
+        print(ve)
         pass
-    except TypeError:
+    except TypeError as te:
+        print(te)
         pass
     else:
         print("{} + {} = {}".format(a, b, add(a, b)))
