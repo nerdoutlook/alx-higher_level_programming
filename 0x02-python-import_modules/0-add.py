43d55340c3c9a4c7e899ce41d2441a51d6da670a
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-04 17:38:35 +0100

    ....

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 from add_0 import add
-
 a = 1
 b = 2
 try:
@@ -12,5 +11,3 @@ except TypeError:
     pass
 else:
     print("{} + {} = {}".format(a, b, add(a, b)))
-if __name__ == "__0-add__":
-    0-add()
