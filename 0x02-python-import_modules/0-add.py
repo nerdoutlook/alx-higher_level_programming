d553b1876cb0b00b4f53899ce3c06b5f21775497
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-04 17:03:52 +0100

    ......

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,7 +3,14 @@ from add_0 import add
 
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+try:
+    a / 1 == a
+    b / 1 == b
+except:
+    pass
+else:
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
-if __name__ == "__main__":
-        main()
+
+#if __name__ == "__main__":
+#   main()
