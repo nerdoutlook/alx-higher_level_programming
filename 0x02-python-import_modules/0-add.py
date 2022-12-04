8d0539b7cd77a6c151f60c688b8992db7362851f
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-04 17:18:18 +0100

    Included try except statement

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,15 +2,13 @@
 from add_0 import add
 
 a = 1
-b = 2
+b = "2"
 try:
     a / 1 == a
     b / 1 == b
-except:
+except ValueError:
+    pass
+except TypeError:
     pass
 else:
     print("{} + {} = {}".format(a, b, add(a, b)))
-
-
-#if __name__ == "__main__":
-#   main()
