762e3e6230e63cbc85db290eda5063289207dbf6
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-04 18:04:01 +0100

    Done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    
     from calculator_1 import add, sub, mul, div
 
     a = 10
@@ -14,7 +13,7 @@ if __name__ == "__main__":
     except TypeError:
         pass
     else:
-        print("{} + {} = {}".format(a, b , add(a, b)))
-        print("{} - {} = {}".format(a, b , sub(a, b)))
-        print("{} * {} = {}".format(a, b , mul(a, b)))
-        print("{} / {} = {}".format(a, b , div(a, b)))
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+        print("{} / {} = {}".format(a, b, div(a, b)))
