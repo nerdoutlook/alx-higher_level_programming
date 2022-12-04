11d80049313ac05a266ad66f4272cf757e13e3c1
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-04 17:47:03 +0100

    Moved if __name__ statement to top of file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from add_0 import add
-a = 1
-b = 2
-try:
-    a / 1 == a
-    b / 1 == b
-except ValueError:
-    pass
-except TypeError:
-    pass
-else:
-    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    try:
+        a / 1 == a
+        b / 1 == b
+    except ValueError:
+        pass
+    except TypeError:
+        pass
+    else:
+        print("{} + {} = {}".format(a, b, add(a, b)))
