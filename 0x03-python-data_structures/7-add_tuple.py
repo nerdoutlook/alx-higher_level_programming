96178e2a05817c9d237c9e1ddd7691644ea994be
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-14 00:38:12 +0100

    Checking

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,23 +5,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     len1 = len(tuple_a)
     len2 = len(tuple_b)
 
-    if len2 == 1:
-        new_tuple_b = tuple_b + dummy
-        for i, j in zip(tuple_a, new_tuple_b):
-            container.append(i + j)
-    elif len2 == 0:
-        new_tuple_b = tuple_b + dummy + dummy
-        for i, j in zip(tuple_a, new_tuple_b):
-            container.append(i + j)
-    elif len1 == 1:
-        new_tuple_a = tuple_a + dummy
-        for i, j in zip(new_tuple_a, tuple_b):
-            container.append(i + j)
-    elif len1 == 0:
-        new_tuple_a = tuple_a + dummy + dummy
-        for i, j in zip(new_tuple_a, tuple_b):
-            container.append(i + j)
-    elif len1 > 2:
+    if len1 > 2:
         for i, j in zip(tuple_a[:2], tuple_b):
             container.append(i + j)
     elif len2 > 2:
