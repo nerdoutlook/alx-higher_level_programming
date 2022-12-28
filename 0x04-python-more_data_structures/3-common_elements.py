be412d1970554038152550cbc9de060e6ccfea35
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-28 04:29:16 +0100

    Adding elements to a set with '.add()' method

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,12 +2,20 @@
 def common_elements(set_1, set_2):
     new_set = set()
     for element in set_1:
+        print(element)
         for element2 in set_2:
+            print(element2)
             if element == element2:
-                new_set = element
+                new_set.add(element)
     return(new_set)
 
 
+"""
+set_1 = { "Python", "Javascript" }
+set_2 = { "Python", "Javascript" }
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
+"""
 """
 set_1 = {"Python", "C", "Javascript"}
 set_2 = {"Bash", "C", "Ruby", "Perl"}
