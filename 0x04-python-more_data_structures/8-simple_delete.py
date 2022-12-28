e7198b69720c5ef932e30e2e7064ed085471156a
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-28 05:05:20 +0100

    removed element in loop sequence before returning dictionary at EOF

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
     if key in a_dictionary:
-        return(a_dictionary.pop(key))
+        a_dictionary.pop(key)
     else:
-        return(a_dictionary)
+        pass
+    return(a_dictionary)
 
 
+"""
+my_dict = { 'a': "a", 'b': "b" , 'c': "c", 'd': "d", 'e': "e" }
+key = 'a'
+"""
 """
 a_dictionary = {'language': "C",
                 'Number': 89,
                 'track': "Low",
                 'ids': [1, 2, 3]}
 #new_dict = simple_delete(a_dictionary, 'language')
-new_dict = simple_delete(a_dictionary, 'l')
+"""
+"""
+new_dict = simple_delete(my_dict, key)
 print(new_dict)
 """
