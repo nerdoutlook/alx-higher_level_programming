a11a059f3dab6e14ad1bec484641c1701b213f84
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-28 04:47:20 +0100

    Checking.......

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -6,11 +6,20 @@ def update_dictionary(a_dictionary, key, value):
 
 
 """
+print_sorted_dictionary = \
+            __import__('6-print_sorted_dictionary').print_sorted_dictionary
+my_dict = { 'a': "a", 'b': "b" , 'c': "c", 'd': "d", 'e': "e" }
+key = "a"
+value = "A"
+new_dict = update_dictionary(my_dict, key, value)
+print_sorted_dictionary(new_dict)
+"""
+"""
 a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
 new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print(new_dict)
+print_sorted_dictionary(new_dict)
 print("-------------")
 new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print(new_dict)
+print_sorted_dictionary(new_dict)
 print("-------------")
 """
