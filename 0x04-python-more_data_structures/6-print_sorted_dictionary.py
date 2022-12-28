86e6b2c0fb57afd04c4bb603a287a36c5d816b9a
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-28 04:36:38 +0100

    Inserted condition to print nothing is dictionary is empty

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for index in sorted(a_dictionary):
+        if a_dictionary is None:
+            print(a_dictionary)
         print("{}: {}".format(index, a_dictionary[index]))
 
-a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+
+"""
+a_dictionary = {}
+a_dictionary = { 'language': "C", 'Number': 89,
+                 'track': "Low level", 'ids': [1, 2, 3] }
 print_sorted_dictionary(a_dictionary)
+"""
