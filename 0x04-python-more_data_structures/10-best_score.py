48fcaea9046ff45acf6cd83679e7328d5d772977
nerdoutlook <vincentuyanwanne@gmail.com>
2022-12-28 05:30:36 +0100

    Done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    max_key = [key for key, value in a_dictionary.items() if value ==
+               max(a_dictionary.values())]
+    if len(max_key) == 1:
+        max_key = max_key[0]
+    return(max_key)
 
 
-
-
-
-
-
-
+"""
 a_dictionary = {'John': 12, 'Bob': 14,
                 'Mike': 14, 'Molly': 16,
-                'Adam': 10}
+                'Adam': 16}
 new = best_score(a_dictionary)
 print(new)
+"""
